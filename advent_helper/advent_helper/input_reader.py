bf8ea8e8f5a17774203cc0e8f4e66d6ba4d12415
David de Bekker <david.de.bekker@gmail.com>
2021-11-30 23:19:30 +0100

    Add read function

--- advent_helper/advent_helper/input_reader.py
+++ advent_helper/advent_helper/input_reader.py
@@ -1,5 +1,10 @@
 import pathlib
-from typing import Callable
+from typing import Any, Callable, Generator
 
-def read(path: pathlib.Path, processor: Callable=None):
-  return None
+def read(
+  path: pathlib.Path,
+  line_processor: Callable[[str], Any]=lambda line: line,
+) -> Generator[str, None, None]:
+  with open(path) as file:
+    while (line := file.readline().rstrip()):
+      yield line_processor(line)
--- solutions/day_01/main.py
+++ solutions/day_01/main.py
@@ -1,4 +1,11 @@
+from os import path
+import pathlib
+
 from advent_helper import input_reader
 
+def get_input_path() -> pathlib.Path:
+  return pathlib.Path(__file__).parent / 'input.txt'
+
+
 if __name__ == '__main__':
-  input_reader.read('test')
+  print([i for i in input_reader.read(get_input_path())])
