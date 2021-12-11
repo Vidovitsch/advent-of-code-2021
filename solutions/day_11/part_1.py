08de34d980f2314f391f882861fa0722e12e97bb
David de Bekker <david.de.bekker@gmail.com>
2021-12-11 10:52:39 +0100

    Finish day 11

--- solutions/day_11/part_1.py
+++ solutions/day_11/part_1.py
@@ -16,14 +16,64 @@ def get_puzzle() -> puzzle.Puzzle:
   )
 
 def run():
-  get_puzzle().run_tests_with(solve)
+  get_puzzle().solve_with(solve, process_input)
 
 ##############################################################
 # Solution
 ##############################################################
 
-def solve(input):
-  return input
+def process_input(line):
+  return [int(char) for char in line]
+
+def solve(energy_level_map):
+  flash_count = 0
+
+  for _ in range(100):
+    add_1(energy_level_map)
+    flash_count += calculate_flash_count(energy_level_map)
+
+  return flash_count
+
+def calculate_flash_count(energy_level_map):
+  flash_count = 0
+
+  for row in range(len(energy_level_map)):
+    for col in range(len(energy_level_map[0])):
+      if energy_level_map[row][col] > 9:
+        flash_count += flash(row, col, energy_level_map)
+
+  return flash_count
+
+def flash(row, col, energy_level_map):
+  if 0 <= row < len(energy_level_map) and 0 <= col < len(energy_level_map[0]):
+    energy_level = energy_level_map[row][col]
+
+    if energy_level == 0:
+      return 0
+    if energy_level <= 9:
+      energy_level_map[row][col] += 1
+
+    if energy_level_map[row][col] > 9:
+      energy_level_map[row][col] = 0
+      adjacents = [
+        flash(row + 1, col, energy_level_map), # top
+        flash(row - 1, col, energy_level_map), # bottom
+        flash(row, col + 1, energy_level_map), # right
+        flash(row, col - 1, energy_level_map), # left
+        flash(row + 1, col + 1, energy_level_map), # top-right
+        flash(row + 1, col - 1, energy_level_map), # top-left
+        flash(row - 1, col + 1, energy_level_map), # bottom-right
+        flash(row - 1, col - 1, energy_level_map) # bottom-left
+      ]
+      return sum(adjacents) + 1
+    return 0
+  else:
+    return 0
+    
+def add_1(energy_level_map):
+  for row in range(len(energy_level_map)):
+    for col in range(len(energy_level_map[0])):
+      energy_level_map[row][col] += 1
 
 if __name__ == '__main__':
   run()
---
+++ solutions/day_11/part_2.py
@@ -0,0 +1,82 @@
+import pathlib
+import math
+
+from advent_helper import puzzle
+
+def get_puzzle() -> puzzle.Puzzle:
+  return puzzle.Puzzle(
+    'Day 11 - Part 2',
+    pathlib.Path(__file__).parent / 'input.txt',
+    [
+      {
+        'input_path': pathlib.Path(__file__).parent / 'test.txt',
+        'expected_result': '195'
+      }
+    ]
+  )
+
+def run():
+  get_puzzle().solve_with(solve, process_input)
+
+##############################################################
+# Solution
+##############################################################
+
+def process_input(line):
+  return [int(char) for char in line]
+
+def solve(energy_level_map):
+  flash_count = 0
+
+  i = 1
+  while True:
+    add_1(energy_level_map)
+    if calculate_flash_count(energy_level_map) == len(energy_level_map) * len(energy_level_map[0]):
+      break
+    i += 1
+
+  return i
+
+def calculate_flash_count(energy_level_map):
+  flash_count = 0
+
+  for row in range(len(energy_level_map)):
+    for col in range(len(energy_level_map[0])):
+      if energy_level_map[row][col] > 9:
+        flash_count += flash(row, col, energy_level_map)
+
+  return flash_count
+
+def flash(row, col, energy_level_map):
+  if 0 <= row < len(energy_level_map) and 0 <= col < len(energy_level_map[0]):
+    energy_level = energy_level_map[row][col]
+
+    if energy_level == 0:
+      return 0
+    if energy_level <= 9:
+      energy_level_map[row][col] += 1
+
+    if energy_level_map[row][col] > 9:
+      energy_level_map[row][col] = 0
+      adjacents = [
+        flash(row + 1, col, energy_level_map), # top
+        flash(row - 1, col, energy_level_map), # bottom
+        flash(row, col + 1, energy_level_map), # right
+        flash(row, col - 1, energy_level_map), # left
+        flash(row + 1, col + 1, energy_level_map), # top-right
+        flash(row + 1, col - 1, energy_level_map), # top-left
+        flash(row - 1, col + 1, energy_level_map), # bottom-right
+        flash(row - 1, col - 1, energy_level_map) # bottom-left
+      ]
+      return sum(adjacents) + 1
+    return 0
+  else:
+    return 0
+    
+def add_1(energy_level_map):
+  for row in range(len(energy_level_map)):
+    for col in range(len(energy_level_map[0])):
+      energy_level_map[row][col] += 1
+
+if __name__ == '__main__':
+  run()
