3449e942b581d2ae1ef4679a49acb26a0aee34ca
David de Bekker <david.de.bekker@gmail.com>
2021-12-11 09:59:00 +0100

    Add input processor to puzzle helper

--- advent_helper/advent_helper/puzzle.py
+++ advent_helper/advent_helper/puzzle.py
@@ -16,19 +16,20 @@ class Puzzle:
   def solve_with(
     self,
     solver: Callable[[List[str]], Any],
+    input_processor: Callable[[str], Any]=lambda x: x,
     run_tests: bool=True
   ) -> str:
     print('--------------------------------------------------------')
 
     if run_tests:
-      if not self.run_tests_with(solver):
+      if not self.run_tests_with(solver, input_processor):
         print()
         raise Exception('One or more tests failed!') 
       print()
 
     print(f'Solving puzzle: {self.name}\n')
     start = timer()
-    solution = str(solver(self._read_input(self.input_path)))
+    solution = str(solver(self._read_input(self.input_path, input_processor)))
     print(f'Solution: {solution} ({round(timer() - start, 10)}s)')
 
     print('--------------------------------------------------------')
@@ -37,20 +38,26 @@ class Puzzle:
 
   def run_tests_with(
     self,
-    solver: Callable[[List[str]], Any]
+    solver: Callable[[List[str]], Any],
+    input_processor: Callable[[str], Any]=lambda x: x,
   ) -> bool:
     print(f'Running tests for puzzle: {self.name}\n')
 
     for test in self.tests:
       print(f"Testing for input: {test['input_path'].name}", end=' ')
-      if not self._run_test_with(test, solver):
+      if not self._run_test_with(test, solver, input_processor):
         return False
 
     print('\nAll tests passed!')
     return True
 
-  def _run_test_with(self, test: Test, solver: Callable[[List[str]], Any]) -> bool:
-    actual = str(solver(self._read_input(test['input_path'])))
+  def _run_test_with(
+    self,
+    test: Test,
+    solver: Callable[[List[str]], Any],
+    input_processor: Callable[[str], Any]
+  ) -> bool:
+    actual = str(solver(self._read_input(test['input_path'], input_processor)))
     expected = test['expected_result']
 
     if actual != expected:
@@ -60,7 +67,11 @@ class Puzzle:
     print('PASSED!')
     return True
 
-  def _read_input(self, path: pathlib.Path) -> List[str]:
+  def _read_input(
+    self,
+    path: pathlib.Path,
+    input_processor: Callable[[str], Any]
+  ) -> List[Any]:
     with open(path) as file:
       lines = (line.rstrip() for line in file)
-      return list(line for line in lines if line)
+      return list(input_processor(line) for line in lines if line)
