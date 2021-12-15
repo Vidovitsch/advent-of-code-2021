f8c41356e2228e361dfa71a2c708822530aab7f8
David de Bekker <david.de.bekker@gmail.com>
2021-12-15 16:38:22 +0100

    Solve day 15

--- solutions/day_15/part_1.py
+++ solutions/day_15/part_1.py
@@ -1,16 +1,44 @@
 import pathlib
 from typing import Any, List
-
+from collections import namedtuple, deque
+import sys
 from advent_helper.puzzle import Puzzle, PuzzleInput
+import heapq
+
+def dijkstra(grid, target, start=(0, 0), risk=0):
+  queue = [(risk, start)]
+  minRisk = {start: risk}
+  visited = {start}
+
+  while queue:
+    risk, (x, y) = heapq.heappop(queue)
+    if (x, y) == target: return risk
+
+    for neighb in ((x+1, y), (x, y+1), (x-1, y), (x, y-1)):
+      if neighb not in grid or neighb in visited: continue
+      visited.add(neighb)
+      newRisk = risk + grid[neighb]
+      if newRisk < minRisk.get(neighb, 999999):
+        minRisk[neighb] = newRisk
+        heapq.heappush(queue, (newRisk, neighb))
 
 def solve(input: PuzzleInput) -> Any:
-  return len(input)
+  maxX, maxY = map(max, zip(*input))
+
+  return dijkstra(input, (maxX, maxY))
 
 def process_input(input: List[str]) -> PuzzleInput:
-  return input
+  processed_input = {}
+
+  for y, row in enumerate(input):
+    for x, n in enumerate(row):
+      processed_input[(x, y)] = int(n)
+
+  return processed_input
 
 if __name__ == '__main__':
   (Puzzle('Day 15 - Part 1', pathlib.Path(__file__).parent / 'input.txt')
     .set_input_processor(process_input)
-    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': -1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': 40 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'input.txt', 'expected_result': 415 })
     .solve(solve))
--- solutions/day_15/part_2.py
+++ solutions/day_15/part_2.py
@@ -1,16 +1,45 @@
 import pathlib
 from typing import Any, List
-
+from collections import namedtuple, deque
+import sys
 from advent_helper.puzzle import Puzzle, PuzzleInput
+import heapq
+
+def dijkstra(grid, target, start=(0, 0), risk=0):
+  queue = [(risk, start)]
+  minRisk = {start: risk}
+  visited = {start}
+
+  while queue:
+    risk, (x, y) = heapq.heappop(queue)
+    if (x, y) == target: return risk
+
+    for neighb in ((x+1, y), (x, y+1), (x-1, y), (x, y-1)):
+      if neighb not in grid or neighb in visited: continue
+      visited.add(neighb)
+      newRisk = risk + grid[neighb]
+      if newRisk < minRisk.get(neighb, 999999):
+        minRisk[neighb] = newRisk
+        heapq.heappush(queue, (newRisk, neighb))
 
 def solve(input: PuzzleInput) -> Any:
-  return len(input)
+  maxX, maxY = map(max, zip(*input))
+
+  return dijkstra(input, (maxX, maxY))
 
 def process_input(input: List[str]) -> PuzzleInput:
-  return input
+  processed_input = {}
+
+  for y, row in enumerate(input):
+    for x, n in enumerate(row):
+      for tile_y in range(5):
+        for tile_x in range(5):
+          processed_input[(x + tile_x * len(row), y + tile_y * len(input))] = (int(n) + tile_y + tile_x) % 9 or 9
+
+  return processed_input
 
 if __name__ == '__main__':
   (Puzzle('Day 15 - Part 2', pathlib.Path(__file__).parent / 'input.txt')
     .set_input_processor(process_input)
-    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': -1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': 315 })
     .solve(solve))
