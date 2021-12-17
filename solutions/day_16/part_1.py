81133c2ce8c613bb394354a7d7ca788f8a7e6321
David de Bekker <david.de.bekker@gmail.com>
2021-12-17 13:25:15 +0100

    Finish day 16

--- solutions/day_16/part_1.py
+++ solutions/day_16/part_1.py
@@ -3,14 +3,83 @@ from typing import Any, List
 
 from advent_helper.puzzle import Puzzle, PuzzleInput
 
-def solve(input: PuzzleInput) -> Any:
-  return len(input)
+def solve(bits: PuzzleInput) -> Any:
+  a = process_packet(bits)[0]
+  return a
+
+def process_packet(bits):
+  version = int(bits[:3], 2)
+  type_id = int(bits[3:6], 2)
+
+  if type_id == 4:
+    value, length = calc_value_and_length(bits)
+    return version, length
+  else:
+    length_type_id = bits[6]
+    if length_type_id == '0':
+      total_length = 22
+      total_versions = version
+      sub_packets_length = int(bits[7:7+15], 2)
+
+      while total_length - 22 != sub_packets_length:
+        sub_packet_version, length = process_packet(bits[total_length:])
+        total_versions += sub_packet_version
+        total_length += length
+      
+      return total_versions, total_length
+    else:
+      num_of_sub_packets = int(bits[7:7+11], 2)
+      total_length = 18
+      total_versions = version
+      for _ in range(num_of_sub_packets):
+        sub_packet_version, length = process_packet(bits[total_length:])
+        total_versions += sub_packet_version
+        total_length += length
+
+      return total_versions, total_length
+
+def calc_value_and_length(bits):
+  literal_value = ''
+  length = 6
+
+  for i in range(6, len(bits), 5):
+    group = bits[i:i + 5]
+    literal_value += group[1:5]
+    length += 5
+
+    if group[0] == '0':
+      break
+
+  return int(literal_value, 2), length
 
 def process_input(input: List[str]) -> PuzzleInput:
-  return input
+  hexadecimal_decoder = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+  }
+
+  return''.join(hexadecimal_decoder[char] for char in input[0])
 
 if __name__ == '__main__':
   (Puzzle('Day 16 - Part 1', pathlib.Path(__file__).parent / 'input.txt')
     .set_input_processor(process_input)
-    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': -1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test1.txt', 'expected_result': 16 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test2.txt', 'expected_result': 12 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test3.txt', 'expected_result': 23 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test4.txt', 'expected_result': 31 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'input.txt', 'expected_result': 923 })
     .solve(solve))
--- solutions/day_16/part_2.py
+++ solutions/day_16/part_2.py
@@ -1,16 +1,106 @@
 import pathlib
 from typing import Any, List
+import numpy as np
 
 from advent_helper.puzzle import Puzzle, PuzzleInput
 
-def solve(input: PuzzleInput) -> Any:
-  return len(input)
+def solve(bits: PuzzleInput) -> Any:
+  a = process_packet(bits)
+  return a[0]
+
+def process_packet(bits):
+  version = int(bits[:3], 2)
+  type_id = int(bits[3:6], 2)
+
+  if type_id == 4:
+    value, length = calc_literal_value_and_length(bits)
+    return value, length
+  else:
+    length_type_id = bits[6]
+    if length_type_id == '0':
+      total_length = 22
+      values = []
+      sub_packets_length = int(bits[7:7+15], 2)
+
+      while total_length - 22 != sub_packets_length:
+        value, length = process_packet(bits[total_length:])
+        values.append(value)
+        total_length += length
+      
+      return calc_value(values, type_id), total_length
+    else:
+      num_of_sub_packets = int(bits[7:7+11], 2)
+      total_length = 18
+      values = []
+      for _ in range(num_of_sub_packets):
+        value, length = process_packet(bits[total_length:])
+        values.append(value)
+        total_length += length
+
+      return calc_value(values, type_id), total_length
+
+def calc_value(values, type_id):
+  if type_id == 0:
+    return sum(values)
+  elif type_id == 1:
+    return np.prod(values)
+  elif type_id == 2:
+    return min(values)
+  elif type_id == 3:
+    return max(values)
+  elif type_id == 5:
+    return 1 if values[0] > values[1] else 0
+  elif type_id == 6:
+    return 1 if values[0] < values[1] else 0
+  elif type_id == 7:
+    return 1 if values[0] == values[1] else 0
+
+def calc_literal_value_and_length(bits):
+  literal_value = ''
+  length = 6
+
+  for i in range(6, len(bits), 5):
+    group = bits[i:i + 5]
+    literal_value += group[1:5]
+    length += 5
+
+    if group[0] == '0':
+      break
+
+  return int(literal_value, 2), length
 
 def process_input(input: List[str]) -> PuzzleInput:
-  return input
+  hexadecimal_decoder = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'A': '1010',
+    'B': '1011',
+    'C': '1100',
+    'D': '1101',
+    'E': '1110',
+    'F': '1111'
+  }
+
+  return''.join(hexadecimal_decoder[char] for char in input[0])
 
 if __name__ == '__main__':
-  (Puzzle('Day 16 - Part 2', pathlib.Path(__file__).parent / 'input.txt')
+  (Puzzle('Day 16 - Part 1', pathlib.Path(__file__).parent / 'input.txt')
     .set_input_processor(process_input)
-    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test.txt', 'expected_result': -1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test5.txt', 'expected_result': 3 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test6.txt', 'expected_result': 54 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test7.txt', 'expected_result': 7 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test8.txt', 'expected_result': 9 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test9.txt', 'expected_result': 1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test10.txt', 'expected_result': 0 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test11.txt', 'expected_result': 0 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'test12.txt', 'expected_result': 1 })
+    .add_test({ 'input_path': pathlib.Path(__file__).parent / 'input.txt', 'expected_result': 258888628940 })
     .solve(solve))
